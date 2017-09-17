0be0dc5c4e1efd61ecf09c35f80c6163a1352528
Luke Rielley <luke.rielley@gmail.com>
2017-09-17 21:55:48 +1000

    Progress on main code loop

--- airauto.ino
+++ airauto.ino
@@ -1,191 +1,208 @@
-
-//      SCK  - Pin 8
-//      MOSI - Pin 9
-//      DC   - Pin 10
-//      RST  - Pin 11
-//      CS   - Pin 12
-//
-
 #include <LCD5110_Graph.h>
 
-LCD5110 myGLCD(8,9,10,11,12);
+// CONFIGURATION VARIABLES
+
+const int LCD_SCK = 8;
+const int LCD_MOSI = 9;
+const int LCD_DC = 10;
+const int LCD_RST = 11;
+const int LCD_CS = 12;
+
+const int RELAY_INFLATE = 3;
+const int RELAY_DEFLATE = 4;
+
+const uint8_t TRANSDUCER_PIN = A0;
+
+// The amount of PSI that are represented by each volt of the transducer
+const float psiPerVolt = 37.5;
+// How many PSI per BAR (to allow unit conversion)
+const float psiPerBar = 14.5038;
+// The time duration (in MS) of the initial inflation
+const int initialInflationTime = 1000;
+// The time duration (in MS) of the initial deflation
+const int initialDeflationTime = 1000;
+// The minimum PSI that we require to determine that a tyre is connected
+const float tyreConnectionMinPsi = 8.0;
+
+LCD5110 myGLCD(LCD_SCK, LCD_MOSI, LCD_DC, LCD_RST, LCD_CS);
 
 extern uint8_t SmallFont[];
 extern uint8_t BigNumbers[];
 
-const float psiPerVolt = 37.5;
-const float psiPerBar = 14.5038;
-
-int readings[50];
-
-/*
-  Indicates that the unit is inflating the tyre to reach the desired pressuure, and that the air inlet valve is open
-*/
+// Indicates the tolerance between the target pressure and the actual pressure
+const float accuracyTolerance = 0.5;
+// Indicates that the unit is inflating the tyre to reach the desired pressuure, and that the air inlet valve is open
 boolean isInflating = false;
-
-/*
-  Indicates that the unit is deflating the tyre to reach the desired pressure, and that the ventilation valve is open
-*/
+// Indicates that the unit is deflating the tyre to reach the desired pressure, and that the ventilation valve is open
 boolean isDeflating = false;
-
-
-/*
-  Indicates that the unit has been told by the user that they want to achieve the target 
-  air pressure. It has been 'switched on'.
-*/
+// Indicates that the unit has been told by the user that they want to achieve the target air pressure. It has been 'switched on'.
 boolean isSwitchedOn = false;
-
-/*
-  Indicates that the unit detcts that it is connected to a tyre. This is generally done by 
-  detecting a pressure above a certain level.
-*/
+// Indicates that the unit detcts that it is connected to a tyre. This is generally done by detecting a pressure above a certain level.
 boolean hasTyreConnected = false;
-
-/* 
-  Indicates that inflating/deflating was attempted, but failed. (The pressure didn't change,
-  indicating a problem with the compressor, a connection, or the tyre) 
-*/
+// Indicates that inflating/deflating was attempted, but failed. (The pressure didn't change, indicating a problem with the compressor, a connection, or the tyre)
 boolean inflationError = false;
-
-
+// Indicates that the pressure reading is erratic. Sensor is not connected properly, or has a failure.
+boolean isPressureReadingErratic = false;
+// Indicates that the tyre is at the set pressure
+boolean isAtSetPressure = false;
+// The target set pressure of the tyre
+float targetPressure = 0;
 
 void setup()
 {
+  // Open Serial connection for debug output
   Serial.begin(9600);
+
+  // Initialise the LCD screen
   myGLCD.InitLCD();
   myGLCD.setFont(SmallFont);
+
+  // Configure the I/O status of the pins we will use to control the solenoid valves
+  pinMode(RELAY_INFLATE, OUTPUT);
+  pinMode(RELAY_DEFLATE, OUTPUT);
 }
 
 /*
   The sensor sends 0.5v for 0PSI, 4.5V for 150ps giving us a 4v range.
   This function will send back an equalised value of 0-4 volts for the sensors total range.
 */
-float getSensorEqualisedVoltage() {
-  int sensorValue = analogRead(A0);
+float getSensorEqualisedVoltage()
+{
+  int sensorValue = analogRead(TRANSDUCER_PIN);
+  Serial.println(F("sensorValue"));
   float voltage = sensorValue * (5.0 / 1023.0);
   return voltage - 0.5;
 }
 
-void error() {
+float getCurrentPsi()
+{
+  return getSensorEqualisedVoltage() * psiPerVolt;
+}
+
+void debugSensor()
+{
+
+  int sensorValue = analogRead(A0);
+
+  Serial.println(F("sensorValue"));
+  Serial.println(sensorValue);
+
+  myGLCD.clrScr();
+  myGLCD.setFont(BigNumbers);
+  myGLCD.printNumI(sensorValue, CENTER, 10);
+
+  myGLCD.update();
+
+  delay(500);
+}
+
+void error()
+{
 
   myGLCD.clrScr();
   myGLCD.print("ERROR!", CENTER, 20);
   myGLCD.update();
-  
-  for (int i=0; i<5; i++)
+
+  for (int i = 0; i < 5; i++)
   {
     myGLCD.invert(true);
     delay(500);
     myGLCD.invert(false);
     delay(500);
   }
-  
 }
 
-// void pumpItUp()
-// {
- 
-//   delay(1000);
+void inflate(int time)
+{
+  Serial.println(F("Opening inflation solenoid"));
+  digitalWrite(RELAY_INFLATE, LOW);
+  Serial.println(F("Inflating"));
+  delay(time);
+  Serial.println(F("Closing inflation solenoid"));
+  digitalWrite(RELAY_INFLATE, HIGH);
+  delay(500);
+}
 
-//   // Reset
-//   isInflating = false;
-//   isDeflating = false;
-
-//   switch(demoMode) {
-//     case 1: {
-//       // Taking Read
-//       isPausedForRead = true;
-//       break;
-//     }
-//     case 2: {
-//       // Deflating
-//       isDeflating = true;
-//       break;
-//     }
-//     case 3: {
-//       // Inflating
-//       isInflating = true;
-//       break;
-//     }
-//     case 4: {
-      
-//       break;
-//     }
-//   }
-  
-  
-//   myGLCD.clrScr();
-  
-  
-
-//   voltage = voltage + calibrate_value;
-
-  
-//   float psi = (voltage - 0.5) * 37.5;
-//   float bar = (psi / psiPerBar);
-
-//   Serial.println(F("Sensor Value:"));
-//   Serial.println(sensorValue);
-//   Serial.println(F("Voltage:"));
-//   Serial.println(voltage);
-  
-//   Serial.println(F("PSI:"));
-//   Serial.println(psi);
-
-//   Serial.println(F("BAR:"));
-//   Serial.println(bar);
-
-// //  myGLCD.print("AUTO AIR", CENTER, 0);
-// //  myGLCD.printNumF(voltage, 5, 1, 10);
-// //  myGLCD.print("VOLTS", 52, 10);
-// //  myGLCD.printNumF(psi, 0, 1, 20);
-// //  myGLCD.print("PSI", 52, 20);
-
-//   myGLCD.print("SET", 40, 10);
-//   myGLCD.print("READ", 0, 10);
-
-//   // myGLCD.print("32 PSI", 40, 0);
-  
-//   if (isInflating) {
-//     myGLCD.drawLine(0,40,20,20);
-//     myGLCD.drawLine(20,20,40,40);
-//     myGLCD.drawLine(0,40,40,40);
-//   }
-
-//   if (isDeflating) {
-//     myGLCD.drawLine(0,20,20,40); // left side
-//     myGLCD.drawLine(20,40,40,20); // right side
-//     myGLCD.drawLine(0,20,40,20); // top line
-//   }
-
-//   if (!isInflating && !isDeflating) {
-//     // Show the pressure reading
-//     // myGLCD.printNumF(psi, 0, 40, 10);
-//       myGLCD.setFont(BigNumbers);
-//       myGLCD.print("22", 0, 20);
-//       myGLCD.setFont(SmallFont);
-//   }
-
-//   myGLCD.setFont(BigNumbers);
-//   myGLCD.print("38", 42, 20);
-//   myGLCD.setFont(SmallFont);
-  
-//   myGLCD.update();
-
-//   demoMode++;
-//   if (demoMode >= 5) {
-//     demoMode = 1;
-//   }
-
-// }
-
+void deflate(int time)
+{
+  Serial.println(F("Opening deflation solenoid"));
+  digitalWrite(RELAY_DEFLATE, LOW);
+  Serial.println(F("Deflating"));
+  delay(time);
+  Serial.println(F("Closing deflation solenoid"));
+  digitalWrite(RELAY_INFLATE, HIGH);
+  delay(500);
+}
 
 void loop()
 {
 
-  // pumpItUp();
+  // We are able to attempt to reach the target pressure
+  if (isSwitchedOn)
+  {
 
+    if (!inflationError)
+    {
+
+      if (!isPressureReadingErratic)
+      {
+
+        float currentPsi = getCurrentPsi();
+        Serial.println(F("Current PST is:"));
+        Serial.println(currentPsi);
+        hasTyreConnected = (currentPsi > tyreConnectionMinPsi);
+
+        if (hasTyreConnected)
+        {
+
+          if (!isAtSetPressure)
+          {
+
+            // positive number means we need to inflate (eg target of 38 and current of 22 gives us 16)
+            float adjustmentRequired = (targetPressure - currentPsi);
+
+            if (adjustmentRequired > accuracyTolerance)
+            {
+              // TODO: Add logic to adjust the inflation time based on an estimation from previous inflation runs for this connection
+              float inflationTime = initialInflationTime;
+              inflate(inflationTime);
+            }
+            else if (adjustmentRequired < (-1 * accuracyTolerance))
+            {
+              // TODO: Add logic to adjust the inflation time based on an estimation from previous deflation runs for this connection
+              float deflationTime = initialDeflationTime;
+              deflate(deflationTime);
+            }
+            else
+            {
+              isAtSetPressure = true;
+            }
+
+          }
+          else
+          {
+            Serial.println(F("Tyre is at set pressure"));
+          }
+        }
+        else
+        {
+          Serial.println(F("No tyre connection was detected"));
+        }
+      }
+      else
+      {
+        Serial.println(F("Pressure reading is erratic. MALFUNCTION"));
+      }
+    }
+    else
+    {
+      Serial.println(F("There is an inflation error"));
+    }
+  }
+  else
+  {
+    Serial.println(F("AirAuto is not switched on"));
+  }
+
+  // debugSensor();
 }
-
-
-
